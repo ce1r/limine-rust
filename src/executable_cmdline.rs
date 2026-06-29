ed393ea65536d8be5506d737eafb0aa7a6142880
ce1r <285079711+ce1r@users.noreply.github.com>
2026-06-29 11:22:25 +0300

    Add executable command line feature

---
+++ src/executable_cmdline.rs
@@ -0,0 +1,39 @@
+use crate::RequestHeader;
+use core::ffi::CStr;
+use core::ffi::c_char;
+
+#[repr(C, align(8))]
+pub struct ExecutableCmdlineRequest {
+    header: RequestHeader<ExecutableCmdlineResponse>,
+}
+
+unsafe impl Send for ExecutableCmdlineRequest {}
+unsafe impl Sync for ExecutableCmdlineRequest {}
+
+impl ExecutableCmdlineRequest {
+    pub const fn new() -> Self {
+        Self {
+            header: RequestHeader::new([0x4b161536e598651e, 0xb390ad4a2f1f303a]),
+        }
+    }
+
+    pub fn response(&self) -> Option<&'static ExecutableCmdlineResponse> {
+        self.header.response()
+    }
+}
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct ExecutableCmdlineResponse {
+    revision: u64,
+    cmdline: *const c_char,
+}
+
+unsafe impl Send for ExecutableCmdlineResponse {}
+unsafe impl Sync for ExecutableCmdlineResponse {}
+
+impl ExecutableCmdlineResponse {
+    pub fn cmdline(&self) -> &str {
+        unsafe { CStr::from_ptr(self.cmdline).to_str().unwrap() }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ pub mod bootloader_performance;
 pub mod date_at_boot;
 pub mod efi_system_table;
 pub mod executable_address;
+pub mod executable_cmdline;
 pub mod executable_file;
 pub mod file;
 pub mod firmware_type;
